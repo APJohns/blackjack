3061d8e119492ac14084d0d849118164e5b431f1
APJohns <ashleypjohns@gmail.com>
2021-04-17 15:22:27 -0400

    Add dealers turn

--- src/App.js
+++ src/App.js
@@ -26,16 +26,26 @@ class App extends React.Component {
       dealerValue: this.getValue(dealerHand),
       currentHand,
       dealerHand
+    }, () => {
+      if (this.state.totalValue === 21) {
+        this.setState({
+          mode: 'blackjack',
+          wallet: this.state.wallet + (this.state.currentBet * 1.5)
+        });
+      }
     });
   }
 
   getEndState = () => {
-    if (this.state.totalValue > 21) {
-      this.setState({ mode: 'bust' });
-    } else if (this.state.dealerValue > 21 || this.state.totalValue > this.state.dealerValue) {
-      this.setState({ mode: 'win' });
+    if (this.state.dealerValue > 21 || this.state.totalValue > this.state.dealerValue) {
+      this.setState({
+        mode: 'win',
+        wallet: this.state.wallet + this.state.currentBet
+      });
     } else if (this.state.dealerValue === this.state.totalValue) {
       this.setState({ mode: 'push' });
+    } else if (this.state.totalValue < this.state.dealerValue) {
+      this.setState({ mode: 'lose' });
     }
   }
 
@@ -45,15 +55,27 @@ class App extends React.Component {
     this.setState({
       totalValue: this.getValue(currentHand),
       currentHand
-    }, this.getEndState);
+    }, () => {
+      if (this.state.totalValue > 21) {
+        this.setState({
+          mode: 'bust',
+          wallet: this.state.wallet - this.state.currentBet
+         });
+      }
+    });
   }
 
   dealersTurn = () => {
     const dealerHand = [...this.state.dealerHand];
-    while (this.dealerValue < 17) {
+    let dealerValue = this.state.dealerValue;
+    while (dealerValue < 17) {
       dealerHand.push(new Card());
+      dealerValue = this.getValue(dealerHand);
     }
-    this.getEndState();
+    this.setState({
+      dealerHand,
+      dealerValue
+    }, this.getEndState);
   }
 
   handleBet = (e) => {
@@ -70,7 +92,7 @@ class App extends React.Component {
       <main>
         <h1>Blackjack</h1>
         {this.state.mode === 'betting' && (
-        <form class="bet-actions" onSubmit={this.submitBet}>
+        <form className="bet-actions" onSubmit={this.submitBet}>
           <label>Bet
             <input type="number" value={this.state.currentBet} onChange={this.handleBet} />
           </label>
@@ -78,7 +100,7 @@ class App extends React.Component {
         </form>
         )}
         {this.state.mode === 'playing' && (
-        <div class="play-actions">
+        <div className="play-actions">
           <button type="button" onClick={this.dealCard}>Hit</button>
           <button type="button" onClick={this.dealersTurn}>Stand</button>
         </div>
@@ -86,6 +108,7 @@ class App extends React.Component {
         {this.state.mode === 'bust' && <p>Bust!</p>}
         {this.state.mode === 'win' && <p>You Win!</p>}
         {this.state.mode === 'push' && <p>Push!</p>}
+        {this.state.mode === 'blackjack' && <p>Blackjack!</p>}
         <h2>Dealer's Hand</h2>
         <dl>
           <dt>Value</dt>
@@ -98,6 +121,8 @@ class App extends React.Component {
         </ul>
         <h2>Your Hand</h2>
         <dl>
+          <dt>Wallet</dt>
+          <dd>{this.state.wallet}</dd>
           <dt>Current Bet</dt>
           <dd>{this.state.currentBet}</dd>
           <dt>Value</dt>
